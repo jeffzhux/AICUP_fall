536ef37ba8b44c608374df3519b73123bbd86acf
jeffzhux <jeffzhux@gmail.com>
2022-11-24 12:38:38 +0800

    2022.11.24 new osda

--- losses/loss.py
+++ losses/loss.py
@@ -22,14 +22,10 @@ class OSDALoss(nn.Module):
         self.t = t
 
     def forward(self, s_logits, t_logits, s_label, t_label):
-        
-        t_softmax = F.softmax(t_logits, dim=1)
-        t_softmax = torch.cat((t_softmax[:,:-1].sum(-1, keepdim=True), t_softmax[:,-1:]), dim=1)
         batch_size = s_logits.size(0)
-        t_label = torch.cat((
-            torch.ones((batch_size,1), device=t_logits.device) * (1-self.t), # know class prob
-            torch.ones((batch_size,1), device=t_logits.device) * self.t      # unknow class prob
-        ), dim=1)
+        # unknow class prob
+        t_softmax = F.softmax(t_logits, dim=1)[:,-1:] # pred
+        t_label = torch.ones((batch_size,1), device=t_logits.device) * self.t # label
         
         trans_loss = self.trans_criterion(t_softmax, t_label)
         cls_loss = self.cls_criterion(s_logits, s_label)
